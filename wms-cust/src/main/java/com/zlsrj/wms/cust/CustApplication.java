b46139565b75de801a648c43291c042cc673f2da
root <378297869@qq.com>
2019-11-20 20:56:53 +0800

    修改项目模块版本号

---
+++ wms-cust/src/main/java/com/zlsrj/wms/cust/CustApplication.java
@@ -0,0 +1,20 @@
+package com.zlsrj.wms.cust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class CustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustApplication.class, args);
+    }
+
+}
