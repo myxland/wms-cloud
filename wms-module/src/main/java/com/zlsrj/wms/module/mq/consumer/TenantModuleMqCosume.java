5d7f5bfd9735b89d4a4adfb72fa5e4779930b958
root <378297869@qq.com>
2019-11-30 20:04:13 +0800

    删除租户模块功能TenantModule

--- wms-module/src/main/java/com/zlsrj/wms/module/mq/consumer/TenantModuleMqCosume.java
+++ wms-module/src/main/java/com/zlsrj/wms/module/mq/consumer/TenantModuleMqCosume.java
@@ -3,12 +3,10 @@ package com.zlsrj.wms.module.mq.consumer;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import com.zlsrj.wms.api.entity.TenantInfo;
-import com.zlsrj.wms.module.service.ITenantModuleAndMenuService;
 
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class TenantModuleMqCosume {
-	
-	@Autowired
-	private ITenantModuleAndMenuService tenantModuleAndMenuService;
 	
 	@JmsListener(destination = "${mq.mbg.tenant.module}")
 	public void receive(TextMessage textMessage) throws JMSException {
@@ -29,7 +24,8 @@ public class TenantModuleMqCosume {
 				TenantInfo tenantInfo = JSONUtil.toBean(text, TenantInfo.class);
 
 				log.info("TenantModuleMqCosume.receive" + "\t" + tenantInfo.toString());
-				tenantModuleAndMenuService.initByTenant(tenantInfo);
+				
+				//当创建新的租户时，分配默认的模块和菜单
 			}
 		}
 
