f1d3c759b0fa758d69d754af3d9b2e71892bb84b
root <378297869@qq.com>
2020-03-11 09:46:11 +0800

    消息队列demo测试代码

--- wms-mq/src/test/java/com/zlsrj/wms/mq/service/base/async/ASyncProduer.java
+++ wms-mq/src/test/java/com/zlsrj/wms/mq/service/base/async/ASyncProduer.java
@@ -6,9 +6,14 @@ import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @Slf4j
 public class ASyncProduer {
 
