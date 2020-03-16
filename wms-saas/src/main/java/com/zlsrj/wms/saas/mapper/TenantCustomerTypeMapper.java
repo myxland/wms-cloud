c25bf3fa69b33ad43922df70053431118a721fa0
root <378297869@qq.com>
2020-03-16 20:08:24 +0800

    修改之前编写的TenantCustomerType相关代码

--- wms-saas/src/main/java/com/zlsrj/wms/saas/mapper/TenantCustomerTypeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.zlsrj.wms.saas.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zlsrj.wms.api.entity.TenantCustomerType;
-
-public interface TenantCustomerTypeMapper extends BaseMapper<TenantCustomerType> {
-
-}
--- wms-saas/src/main/java/com/zlsrj/wms/saas/service/ITenantCustomerTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.zlsrj.wms.saas.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zlsrj.wms.api.entity.TenantCustomerType;
-import com.zlsrj.wms.api.entity.TenantInfo;
-
-public interface ITenantCustomerTypeService extends IService<TenantCustomerType> {
-
-}
