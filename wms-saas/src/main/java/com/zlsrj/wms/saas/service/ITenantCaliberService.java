9100648fa09e428fc46b129ad62aa8d7b0dcedc5
root <378297869@qq.com>
2020-03-26 09:47:16 +0800

    去掉不存在实体对应类TenantCaliber及相关操作类

--- wms-saas/src/main/java/com/zlsrj/wms/saas/service/ITenantCaliberService.java
+++
@@ -1,9 +0,0 @@
-package com.zlsrj.wms.saas.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zlsrj.wms.api.entity.TenantCaliber;
-import com.zlsrj.wms.api.entity.TenantInfo;
-
-public interface ITenantCaliberService extends IService<TenantCaliber> {
-
-}
