efb3007906642dbc55c8b5addcf0bbcad4a01bf1
root <378297869@qq.com>
2020-03-31 17:36:54 +0800

    删除TenantCustomer历史相关功能

--- wms-saas/src/main/java/com/zlsrj/wms/saas/service/ITenantCustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.zlsrj.wms.saas.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zlsrj.wms.api.entity.TenantCustomer;
-import com.zlsrj.wms.api.entity.TenantInfo;
-
-public interface ITenantCustomerService extends IService<TenantCustomer> {
-
-}
