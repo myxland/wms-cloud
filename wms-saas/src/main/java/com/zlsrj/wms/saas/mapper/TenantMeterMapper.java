83114e5d144abf05290381acdaad5794c5064f82
root <378297869@qq.com>
2020-04-01 14:21:31 +0800

    删除历史关于TenantMeter相关功能

--- wms-saas/src/main/java/com/zlsrj/wms/saas/mapper/TenantMeterMapper.java
+++
@@ -1,8 +0,0 @@
-package com.zlsrj.wms.saas.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zlsrj.wms.api.entity.TenantMeter;
-
-public interface TenantMeterMapper extends BaseMapper<TenantMeter> {
-
-}
--- wms-saas/src/main/java/com/zlsrj/wms/saas/service/ITenantMeterService.java
+++
@@ -1,9 +0,0 @@
-package com.zlsrj.wms.saas.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zlsrj.wms.api.entity.TenantMeter;
-import com.zlsrj.wms.api.entity.TenantInfo;
-
-public interface ITenantMeterService extends IService<TenantMeter> {
-
-}
